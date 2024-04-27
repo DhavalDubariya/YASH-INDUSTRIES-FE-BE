a8e08e7bfad3878c64c3a11c8ac70672a08a06b6
Dhaval <dhavaldubariya35@gmail.com>
2024-04-27 20:31:48 +0530

    Updated Code

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -31,6 +31,7 @@ $(document).ready(async function() {
         success: function(response) {
             if(response.status == true){
                 console.log('Product Detail')
+                setUpdateProduct(response.data)
             }
         },
         error: function(xhr, status, error) {
@@ -177,3 +178,48 @@ $('#customer-product').on('click',async function(e){
         }
     });
 })
+
+function setUpdateProduct(productData) {
+    $('#product_name').val(productData.product_name)
+    $('#product_quantity').val(productData.product_qty)
+    $('#runner').val(productData.runner)
+    $('#product-page').text('Update Product')
+    $('#customer-product').text('Update')
+    $('#customer-product').attr('flag-create',"false")
+    var date = new Date().getTime()
+    var materialHtml = ``
+    for(let i=0;i<productData.material.length;i++){
+
+        materialHtml = materialHtml + `
+        <tr style="padding: 0px !important;" material-data-id="${date + parseInt(Math.random()*1000)}">
+        <form id="${date}_material" class="needs-validation" novalidate="">
+            <td style="padding: 0px !important;">
+            <input style="border-radius: 0px !important;" class="form-control" type="text" name="material_name" id="material_name" placeholder="Name" value="${productData.material[i].material_name}"
+                required />
+            <div class="valid-feedback">Looks good!</div>
+            <div class="invalid-feedback">Please provide material name.</div>
+            </td>
+            <td style="padding: 0px !important;">
+            <input style="border-radius: 0px !important;" class="form-control" type="text" name="material_color" id="material_color" placeholder="Color" value="${productData.material[i].material_color}"
+                required />
+            <div class="valid-feedback">Looks good!</div>
+            <div class="invalid-feedback">Please provide color.</div>
+            </td style="padding: 0px !important;">
+            <td style="padding: 0px !important;"><input style="border-radius: 0px !important;" class="form-control" type="number" pattern="^[1-9]\d*$" name="material_qty" id="material_qty" placeholder="Quantity" value="${productData.material[i].material_qty}"
+            required />
+            <div class="valid-feedback">Looks good!</div>
+            <div class="invalid-feedback">Please provide quantity.</div></td>
+            <td style="padding: 0.325rem; cursor: pointer;" onclick="removeMaterial(${date +parseInt(Math.random()*1000) })">
+            <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-minus-circle text-danger" style="height: 18px; width: 18px;"><circle cx="12" cy="12" r="10"></circle><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+            </div>
+            </td>
+        </form>
+        </tr>
+        `
+    }
+
+    // playSound(true)
+    $("#material").empty()
+    $("#material").append(materialHtml)
+}
